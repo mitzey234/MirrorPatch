e74a617a1ae6ab0cbbeec26e438a3ab6b8cd702c
Alexander Young <alex1001@live.ca>
2023-01-16 04:36:38 -0700

    Everything
    This is a really basic plugin. Keep in mind it requires the use of Harmony

---
+++ MirrorPatch/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using PluginAPI.Core.Attributes;
+using System;
+
+namespace BrightPlugin
+{
+    public class Plugin
+    {
+        public static Plugin Singleton { get; private set; }
+        public static Harmony Harmony { get; private set; }
+        
+        [PluginEntryPoint("Mirror Patch", "1.0.0", "Repairs a potential exploit in Mirror that breaks the server", "Mitzey")]
+        void LoadPlugin()
+        {
+            Singleton = this;
+            Harmony = new Harmony($"mirrorPatch-{DateTime.Now.Ticks}");
+            Harmony.PatchAll();
+        }
+        
+        [PluginUnload]
+        void UnloadPlugin()
+        {
+            Harmony.UnpatchAll(Harmony.Id);
+            Harmony = null;
+        }
+    }
+}
---
+++ MirrorPatch/TransportReceivePatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using Mirror;
+using PluginAPI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrightPlugin.Utils
+{
+    //For some reaosn its possible to make the server disconnect itself with invalid data, this patch stops that from being a thing
+    [HarmonyPatch(typeof(NetworkConnection), nameof(NetworkConnection.TransportReceive))]
+    internal class TransportReceivePatch
+    {
+        public static bool Prefix(ArraySegment<byte> buffer, int channelId, NetworkConnection __instance)
+        {
+            if (buffer.Count < 2)
+            {
+                if (__instance.identity != null && __instance.identity.isServer) return false;
+                UnityEngine.Debug.LogError(string.Format("ConnectionRecv {0} Message was too short (messages should start with message id)", __instance));
+                Log.Debug("Caught dumb shit: " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
+                __instance.Disconnect();
+                return false;
+            }
+            using (PooledNetworkReader reader = NetworkReaderPool.GetReader(buffer))
+            {
+                while (reader.Position < reader.Length)
+                {
+                    if (!__instance.UnpackAndInvoke(reader, channelId))
+                    {
+                        break;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}

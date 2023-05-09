8b8917aff14ce99b75cf7c6d45eb468b80c5073b
Alexander Young <alex1001@live.ca>
2023-05-09 10:26:30 -0700

    Probably works
    UNTESTED, use at own risk

--- MirrorPatch/TransportReceivePatch.cs
+++ MirrorPatch/TransportReceivePatch.cs
@@ -6,33 +6,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace BrightPlugin.Utils
 {
-    //For some reaosn its possible to make the server disconnect itself with invalid data, this patch stops that from being a thing
-    [HarmonyPatch(typeof(NetworkConnection), nameof(NetworkConnection.TransportReceive))]
+    //For some reason its possible to make the server disconnect itself with invalid data, this patch stops that from being a thing
+    [HarmonyPatch(typeof(NetworkClient), nameof(NetworkClient.OnTransportData))]
     internal class TransportReceivePatch
     {
-        public static bool Prefix(ArraySegment<byte> buffer, int channelId, NetworkConnection __instance)
+        public static bool Prefix(ArraySegment<byte> data, int channelId, NetworkConnection __instance)
         {
-            if (buffer.Count < 2)
+            if (NetworkClient.connection != null)
             {
-                if (__instance.identity != null && __instance.identity.isServer) return false;
-                UnityEngine.Debug.LogError(string.Format("ConnectionRecv {0} Message was too short (messages should start with message id)", __instance));
-                Log.Debug("Caught dumb shit: " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
-                __instance.Disconnect();
-                return false;
-            }
-            using (PooledNetworkReader reader = NetworkReaderPool.GetReader(buffer))
-            {
-                while (reader.Position < reader.Length)
+                if (!NetworkClient.unbatcher.AddBatch(data))
                 {
-                    if (!__instance.UnpackAndInvoke(reader, channelId))
+                    Debug.LogWarning((object) "NetworkClient: failed to add batch, disconnecting.");
+                    if (__instance.identity != null && __instance.identity.isServer)
                     {
-                        break;
+                        Log.Debug("NetworkClient ERROR (unbatcher.AddBatch): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
+                        return false;
                     }
+                    NetworkClient.connection.Disconnect();
+                }
+                else
+                {
+                    NetworkReader message;
+                    double remoteTimeStamp;
+                    while (!NetworkClient.isLoadingScene && NetworkClient.unbatcher.GetNextMessage(out message, out remoteTimeStamp))
+                    {
+                        if (message.Remaining >= 2)
+                        {
+                            NetworkClient.connection.remoteTimeStamp = remoteTimeStamp;
+                            if (!NetworkClient.UnpackAndInvoke(message, channelId))
+                            {
+                                Debug.LogWarning((object) "NetworkClient: failed to unpack and invoke message. Disconnecting.");
+                                if (__instance.identity != null && __instance.identity.isServer)
+                                {
+                                    Log.Debug("NetworkClient ERROR (UnpackAndInvoke): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
+                                    return false;
+                                }
+                                NetworkClient.connection.Disconnect();
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            if (__instance.identity != null && __instance.identity.isServer) return false;
+                            Debug.LogWarning((object) "NetworkClient: received Message was too short (messages should start with message id)");
+                            if (__instance.identity != null && __instance.identity.isServer)
+                            {
+                                Log.Debug("NetworkClient ERROR (message.Remaining): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
+                                return false;
+                            }
+                            NetworkClient.connection.Disconnect();
+                            return false;
+                        }
+                    }
+                    if (NetworkClient.isLoadingScene || NetworkClient.unbatcher.BatchesCount <= 0) return false;
+                    Debug.LogError((object) string.Format("Still had {0} batches remaining after processing, even though processing was not interrupted by a scene change. This should never happen, as it would cause ever growing batches.\nPossible reasons:\n* A message didn't deserialize as much as it serialized\n*There was no message handler for a message id, so the reader wasn't read until the end.", (object) NetworkClient.unbatcher.BatchesCount));
                 }
             }
+            else
+                Debug.LogError((object) "Skipped Data message handling because connection is null.");
+
             return false;
         }
     }

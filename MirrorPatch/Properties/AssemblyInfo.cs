1a418dcf4d9ee2be330e6bd14bbf018d824b6182
Alexander Young <alex1001@live.ca>
2023-06-21 11:13:31 -0700

    Update Version Number

--- MirrorPatch/Properties/AssemblyInfo.cs
+++ MirrorPatch/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BrightPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MirrorPatch")]
+[assembly: AssemblyDescription("A NWAPI Plugin for fixing mirror / blocking mirror from disconnecting server instance")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mitzey")]
 [assembly: AssemblyProduct("BrightPlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]

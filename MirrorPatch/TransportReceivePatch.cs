dcdd959fa16b478e139bb9b80e715b71fc096b85
Alexander Young <alex1001@live.ca>
2023-07-03 14:47:49 -0700

    Probably fixed a bug

--- MirrorPatch/TransportReceivePatch.cs
+++ MirrorPatch/TransportReceivePatch.cs
@@ -20,55 +20,53 @@ namespace MirrorPatch.Utils
             {
                 if (!NetworkClient.unbatcher.AddBatch(data))
                 {
-                    Debug.LogWarning((object) "NetworkClient: failed to add batch, disconnecting.");
+                    Debug.LogWarning("NetworkClient: failed to add batch, disconnecting.");
                     if (__instance.identity != null && __instance.identity.isServer)
                     {
                         Log.Debug("NetworkClient ERROR (unbatcher.AddBatch): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
                         return false;
                     }
                     NetworkClient.connection.Disconnect();
+                    return false;
                 }
-                else
+                NetworkReader networkReader;
+                double num;
+                while (!NetworkClient.isLoadingScene && NetworkClient.unbatcher.GetNextMessage(out networkReader, out num))
                 {
-                    NetworkReader message;
-                    double remoteTimeStamp;
-                    while (!NetworkClient.isLoadingScene && NetworkClient.unbatcher.GetNextMessage(out message, out remoteTimeStamp))
+                    if (networkReader.Remaining < 2)
                     {
-                        if (message.Remaining >= 2)
+                        Debug.LogWarning("NetworkClient: received Message was too short (messages should start with message id)");
+                        if (__instance.identity != null && __instance.identity.isServer)
                         {
-                            NetworkClient.connection.remoteTimeStamp = remoteTimeStamp;
-                            if (!NetworkClient.UnpackAndInvoke(message, channelId))
-                            {
-                                Debug.LogWarning((object) "NetworkClient: failed to unpack and invoke message. Disconnecting.");
-                                if (__instance.identity != null && __instance.identity.isServer)
-                                {
-                                    Log.Debug("NetworkClient ERROR (UnpackAndInvoke): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
-                                    return false;
-                                }
-                                NetworkClient.connection.Disconnect();
-                                return false;
-                            }
-                        }
-                        else
-                        {
-                            if (__instance.identity != null && __instance.identity.isServer) return false;
-                            Debug.LogWarning((object) "NetworkClient: received Message was too short (messages should start with message id)");
-                            if (__instance.identity != null && __instance.identity.isServer)
-                            {
-                                Log.Debug("NetworkClient ERROR (message.Remaining): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
-                                return false;
-                            }
-                            NetworkClient.connection.Disconnect();
+                            Log.Debug("NetworkClient ERROR (unbatcher.GetNextMessage): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
                             return false;
                         }
+                        NetworkClient.connection.Disconnect();
+                        return false;
                     }
-                    if (NetworkClient.isLoadingScene || NetworkClient.unbatcher.BatchesCount <= 0) return false;
-                    Debug.LogError((object) string.Format("Still had {0} batches remaining after processing, even though processing was not interrupted by a scene change. This should never happen, as it would cause ever growing batches.\nPossible reasons:\n* A message didn't deserialize as much as it serialized\n*There was no message handler for a message id, so the reader wasn't read until the end.", (object) NetworkClient.unbatcher.BatchesCount));
+                    NetworkClient.connection.remoteTimeStamp = num;
+                    if (!NetworkClient.UnpackAndInvoke(networkReader, channelId))
+                    {
+                        Debug.LogWarning("NetworkClient: failed to unpack and invoke message. Disconnecting.");
+                        if (__instance.identity != null && __instance.identity.isServer)
+                        {
+                            Log.Debug("NetworkClient ERROR (unbatcher.UnpackAndInvoke): " + __instance.address + " - " + (__instance.identity != null ? __instance.identity.name + " - " + __instance.identity.isServer : "(null)") + __instance.connectionId + " - " + __instance.isAuthenticated);
+                            return false;
+                        }
+                        NetworkClient.connection.Disconnect();
+                        return false;
+                    }
+                }
+                if (!NetworkClient.isLoadingScene && NetworkClient.unbatcher.BatchesCount > 0)
+                {
+                    Debug.LogError(string.Format("Still had {0} batches remaining after processing, even though processing was not interrupted by a scene change. This should never happen, as it would cause ever growing batches.\nPossible reasons:\n* A message didn't deserialize as much as it serialized\n*There was no message handler for a message id, so the reader wasn't read until the end.", NetworkClient.unbatcher.BatchesCount));
+                    return false;
                 }
             }
             else
-                Debug.LogError((object) "Skipped Data message handling because connection is null.");
-
+            {
+                Debug.LogError("Skipped Data message handling because connection is null.");
+            }
             return false;
         }
     }
